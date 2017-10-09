70384dee87087225b1bd6d520035c5871de6a4a0
mravendi <mra446@gmail.com>
2017-10-08 22:28:05 -0700

    time planner

---
+++ timePlanner.py
@@ -0,0 +1,59 @@
+'''
+Implement a function meetingPlanner that given the availability, slotsA and slotsB, of two people and a meeting duration dur, returns the earliest time slot that works for both of them and is of duration dur. If there is no common time slot that satisfies the duration requirement, return null.
+
+Time is given in a Unix format called Epoch, which is a nonnegative integer holding the number of seconds that have elapsed since 00:00:00 UTC, Thursday, 1 January 1970.
+
+Each person’s availability is represented by an array of pairs. Each pair is an epoch array of size two. The first epoch in a pair represents the start time of a slot. The second epoch is the end time of that slot. The input variable dur is a positive integer that represents the duration of a meeting in seconds. The output is also a pair represented by an epoch array of size two.
+
+In your implementation assume that the time slots in a person’s availability are disjointed, i.e, time slots in a person’s availability don’t overlap. Further assume that the slots are sorted by slots’ start time.
+
+Implement an efficient solution and analyze its time and space complexities.
+
+Examples:
+
+input:  slotsA = [[10, 50], [60, 120], [140, 210]]
+        slotsB = [[0, 15], [60, 70]]
+        dur = 8
+output: [60, 68]
+
+input:  slotsA = [[10, 50], [60, 120], [140, 210]]
+        slotsB = [[0, 15], [60, 70]]
+        dur = 12
+output: null # since there is no common slot
+
+'''
+#%%
+
+def meeting_planner(slotsA, slotsB, dur):
+
+  i,j=0,0
+  nA=len(slotsA) # 1
+  nB=len(slotsB) # 1
+  while i<nA and j<nB: # i=1,j=1
+  
+    start = max(slotsA[i][0], slotsB[j][0]) # 7
+    end = min(slotsA[i][1], slotsB[j][1]) # 11
+    
+    if start + dur <= end: # check starting values
+      return [start, start + dur] # [60,68]
+    elif slotsA[i][1] < slotsB[j][1]: 
+      i+=1
+    else:  
+      j+=1
+    
+  return []  
+  
+  
+
+slotsA = [[10, 50], [60, 120], [140, 210]]
+slotsB = [[0, 15], [60, 70]]
+dur = 8
+
+slotsA = [[10, 50], [60, 120], [140, 210]]
+slotsB = [[0, 15], [60, 70]]
+dur = 12
+
+slotsA= [[7,12]]
+slotsB= [[2,11]]
+dur= 5
+print meeting_planner(slotsA, slotsB, dur)  
